ff53dfbc73dd74e90832fa00a87106bfb2dcf9b2
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-04-04 01:26:47 +0500

    feat(setuped project): imported assets, setted library, mocked data

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
