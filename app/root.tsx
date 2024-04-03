e91a7ac32bcfc37a07190b832a2c87011574b016
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-04-04 01:30:12 +0500

    fix(html): added icon

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
